6415a0d1a860a7caf8f7f6f992d901fc27eb0491
Serge Fonville <serge.fonville@gmail.com>
2018-02-12 00:41:11 +0100

    Added a navigation bar

--- index.php
+++ index.php
@@ -10,14 +10,34 @@
 			integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
 			crossorigin="anonymous"
 		>
+		<script
+			src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+			integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+			crossorigin="anonymous"
+		>
+		</script>
 		<script src="Scripts/mealRegistrationController.js"></script>
 	</head>
 	<body>
-		<a href="#/">Main</a>
-		<a href="#/add">Add</a>
-		<a href="#/show">Show</a>
-		<a href="#/list">List</a>
-		<a href="#/edit">Edit</a>
+		<nav class="navbar navbar-expand-sm bg-light navbar-light">
+			<ul class="nav navbar-nav">
+				<li class="nav-item active">
+					<a class="nav-link" href="#/">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#/add">Add</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#/edit">Edit</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#/list">List</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#/show">Show</a>
+				</li>
+			</ul>
+		</nav>
 		<div ng-view></div>
 	</body>
 </html>
