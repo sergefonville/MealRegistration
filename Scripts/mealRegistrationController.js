03be004d53120de7e269e2b7f1baf57e767ddb96
Serge Fonville <serge.fonville@gmail.com>
2018-02-12 18:40:52 +0100

    Started implementing the Add page

--- Scripts/mealRegistrationController.js
+++ Scripts/mealRegistrationController.js
@@ -1,3 +1,6 @@
-app.controller('mealRegistrationController', ['$scope', function($scope) {
-	
+app.controller('mealRegistrationController', ['$scope', '$http', function($scope, $http) {
+	$scope.foodItems = [];
+	$scope.addFoodItem = function() {
+		$scope.foodItems.push();
+	};
 }]);
