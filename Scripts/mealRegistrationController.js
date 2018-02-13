c926e00e1a8071ade8e1b931470b4b3da55e1511
Serge Fonville <serge.fonville@gmail.com>
2018-02-13 13:05:05 +0100

    Added $scope.units to mealRegistrationController.js

--- Scripts/mealRegistrationController.js
+++ Scripts/mealRegistrationController.js
@@ -1,5 +1,8 @@
 app.controller('mealRegistrationController', ['$scope', '$http', function($scope, $http) {
 	$scope.foodItems = [];
+	$scope.units = [];
+	$scope.units.push('Grams');
+	$scope.units.push('Kcal');
 	$scope.addFoodItem = function() {
 		$scope.foodItems.push({});
 	};
