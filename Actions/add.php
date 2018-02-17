e56d5ff8bddaf6eea872470269f148115024025a
Serge Fonville <serge.fonville@gmail.com>
2018-02-17 22:01:45 +0100

    Started JSON interpretation of the data of the request

--- Actions/add.php
+++ Actions/add.php
@@ -1,5 +1,8 @@
 <?php
-	if($_SERVER['REQUEST_METHOD'] !== 'PUT') {
+	if($_SERVER['REQUEST_METHOD'] !== 'PUT')
 		die("Invalid Request");
-	}
+
+	$data = file_get_contents('php://input');
+	$foodItems = json_decode($data);
+	
 ?>
