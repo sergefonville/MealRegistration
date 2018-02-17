b5f7d1be5bac09dd68942dbf4d64edf957763ef5
Serge Fonville <serge.fonville@gmail.com>
2018-02-17 23:26:36 +0100

    Added classes for handling meals

--- Actions/add.php
+++ Actions/add.php
@@ -1,7 +1,8 @@
 <?php
 	if($_SERVER['REQUEST_METHOD'] !== 'PUT')
 		die("Invalid Request");
-
+	require_once('mealRegistration.class.php');
+	$mealRegistration = new MealRegistration();
 	$data = file_get_contents('php://input');
 	$foodItems = json_decode($data);
 	
---
+++ Classes/Meal.class.php
@@ -0,0 +1,6 @@
+<?php
+	class Meal {
+		public $Timestamp;
+		public $FoodItems;
+	}
+?>
