c4a3e1434540b22f7e2f19e687742b5ffcdd62f1
Serge Fonville <serge.fonville@gmail.com>
2018-02-20 11:23:35 +0100

    Removed code duplication by creating mealRegistration.include.php and updating addmeal.php and addfooditem.php

--- Actions/addmeal.php
+++ Actions/addmeal.php
@@ -1,9 +1,5 @@
 <?php
-	if($_SERVER['REQUEST_METHOD'] !== 'PUT')
-		die("Invalid Request");
-	require_once('mealRegistration.class.php');
-	$mealRegistration = new MealRegistration();
-	$data = file_get_contents('php://input');
-	$foodItems = json_decode($data);
-	
+	require_once('../Includes/mealregistration.include.php');
+	$meal = json_decode($data);
+	$mealRegistration->addMeal($meal);
 ?>
---
+++ Includes/mealRegistration.include.php
@@ -0,0 +1,7 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] !== 'PUT')
+		die("Invalid Request");
+	require_once('../Classes/mealRegistration.class.php');
+	$mealRegistration = new MealRegistration();
+    $data = file_get_contents('php://input');
+?>
