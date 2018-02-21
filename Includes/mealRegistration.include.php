e05a412c099585df0ae50ee3075c67f5ef11b3f2
Serge Fonville <serge.fonville@gmail.com>
2018-02-21 22:11:29 +0100

    Started adding functions to convert json to objects

--- Includes/mealRegistration.include.php
+++ Includes/mealRegistration.include.php
@@ -4,4 +4,15 @@ if($_SERVER['REQUEST_METHOD'] !== 'PUT')
   require_once('../Classes/MealRegistration.class.php');
   $mealRegistration = new MealRegistration();
   $data = file_get_contents('php://input');
+  function to_meal($json) {
+    $object = json_decode($json);
+    $meal = new Meal();
+    $meal->Timestamp = "";
+    $meal->FoodItems = array();
+  }
+
+  function to_fooditem($json) {
+
+  }
+
 ?>
